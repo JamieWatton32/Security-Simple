b067baf69edf7c4955f22f169a98728950cd721c
JamieWatton32 <jamiewatton199559955@gmail.com>
2024-06-17 18:06:20 -0300

    added some logic for creating MP

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,8 @@
 use egui::{FontId, Response, RichText};
+use rusqlite::types::FromSqlError;
 
 use crate::{
-    areas::{MasterArea, RegularArea},
+    areas::{DbErr, MasterArea, RegularArea},
     key::Key,
 };
 
@@ -12,25 +13,23 @@ pub struct Database {
     regular: RegularArea,
     inputs: Inputs,
 }
+
+//For keepign track of the current string inside of text boxs,
+//needed because e
 #[derive(serde::Deserialize, serde::Serialize, Debug)]
 pub struct Inputs {
     site_current: String,
     password_current: String,
+    master_current: String,
 }
-
 impl Inputs {
     fn empty() -> Inputs {
         Inputs {
             site_current: String::new(),
             password_current: String::new(),
+            master_current: String::new(),
         }
     }
-}
-
-#[derive(serde::Deserialize, serde::Serialize, Debug)]
-pub struct Decrypt {
-    pub site: String,
-    pub password: String,
 }
 
 impl Database {
@@ -47,19 +46,49 @@ impl Database {
 impl eframe::App for Database {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            let test_password: &str = "test123";
-            let _ = self.master.add_master(test_password);
-            ui.label(RichText::new("Enter master password").font(FontId::proportional(40.0)));
+            if !self.master.created {
+                ui.label(
+                    RichText::new("Please create masterword.").font(FontId::proportional(40.0)),
+                );
+                ui.label(
+                    RichText::new("If lost there is no way to recover this!")
+                        .font(FontId::proportional(40.0)),
+                );
+                let response_mp: Response = ui.add(
+                    egui::TextEdit::singleline(&mut self.master.password)
+                        .font(FontId::proportional(20.0)),
+                );
+                if response_mp.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
+                    match self.master.add_master(&self.master.password) {
+                        Ok(_) => {
+                            ui.label(
+                                RichText::new("Created succesfully!")
+                                    .font(FontId::proportional(40.0)),
+                            );
+                            self.master.created = true;
+                        }
+                        Err(_) => {
+                            ui.label(
+                                RichText::new("Incorrect password. Try again!")
+                                    .font(FontId::proportional(40.0)),
+                            );
+                        }
+                    }
+                }
+            }
 
-            let response: Response = ui.add(egui::TextEdit::password(
-                egui::TextEdit::singleline(&mut self.master.password)
-                    .font(FontId::proportional(20.0)),
-                true,
-            ));
+            if !self.master.passed && self.master.created {
+                ui.label(RichText::new("Enter master password").font(FontId::proportional(40.0)));
+                let response: Response = ui.add(egui::TextEdit::password(
+                    egui::TextEdit::singleline(&mut self.master.password)
+                        .font(FontId::proportional(20.0)),
+                    true,
+                ));
 
-            if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
-                if self.master.extract_master() == self.master.password {
-                    self.master.passed = true;
+                if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
+                    if self.master.extract_master() == self.master.password {
+                        self.master.passed = true;
+                    }
                 }
             }
 
@@ -75,11 +104,25 @@ impl eframe::App for Database {
 
                 ui.horizontal(|ui| {
                     ui.heading("Enter password for the above site");
-                    ui.text_edit_singleline(&mut self.inputs.password_current)
-                        .enabled();
+                    if ui
+                        .button(
+                            RichText::new("Generate random password?")
+                                .font(FontId::proportional(10.0)),
+                        )
+                        .clicked()
+                    {
+                        self.inputs.password_current = self.regular.generate_password()
+                            + "-"
+                            + &self.regular.generate_password();
+                    } else {
+                        ui.text_edit_singleline(&mut self.inputs.password_current);
+                    }
                 });
 
-                if ui.button("Add to database").clicked() {
+                if ui
+                    .button(RichText::new("Add password!").font(FontId::proportional(20.0)))
+                    .clicked()
+                {
                     if let Err(err) = self
                         .regular
                         .add_regular(&self.inputs.site_current, &self.inputs.password_current)
@@ -98,30 +141,22 @@ impl eframe::App for Database {
                 }
 
                 ui.heading("Stored Data");
+                ui.separator();
                 egui::ScrollArea::horizontal().show(ui, |ui| {
-                    for site in &self.regular.sites {
+                    for (idx, pw) in self.regular.passwords.iter().enumerate() {
                         ui.horizontal(|ui| {
+                            ui.separator();
                             ui.label(
-                                RichText::new(format!("Site Name: {}", site))
-                                    .font(FontId::proportional(20.0)),
+                                RichText::new(format!(
+                                    "Password for {}: {}",
+                                    self.regular.sites[idx], pw
+                                ))
+                                .font(FontId::proportional(20.0)),
                             );
-
-                            
-                        });
-                    }
-                    for pw in &self.regular.passwords {
-                        ui.horizontal(|ui| {
-                            ui.label(
-                                RichText::new(format!("Site Name: {}", pw))
-                                    .font(FontId::proportional(20.0)),
-                            );
-
-                            
                         });
                     }
                 });
             }
         });
-     
     }
 }
